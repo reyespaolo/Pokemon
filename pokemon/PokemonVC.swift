45b8fa828f827cf9b3b67f12ae8e17825869b251
Paolo Reyes <reyes.paolo@360logix.com>
2017-03-25 19:49:11 +0800

    Display Pokemon from CSV File

---
+++ pokemon/PokemonVC.swift
@@ -0,0 +1,73 @@
+//
+//  ViewController.swift
+//  pokemon
+//
+//  Created by Paolo Reyes on 25/03/2017.
+//  Copyright © 2017 360 Logix Solutions Inc. All rights reserved.
+//
+
+import UIKit
+
+class PokemonVC: UIViewController, UICollectionViewDelegate, UICollectionViewDataSource, UICollectionViewDelegateFlowLayout {
+
+    @IBOutlet weak var collection: UICollectionView!
+    
+    var pokemon = [Pokemon]()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        collection.dataSource = self
+        collection.delegate = self
+        parsePokemonCSV()
+    }
+
+    func parsePokemonCSV(){
+        let path = Bundle.main.path(forResource: "pokemon", ofType: "csv")!
+        
+        do{
+            let csv = try CSV(contentsOfURL: path)
+            let rows = csv.rows
+
+            for row in rows {
+                let pokeId = Int(row["id"]!)!
+                let name = row["identifier"]!
+                let poke =  Pokemon(name: name, pokedexId: pokeId)
+                pokemon.append(poke)
+                
+            }
+        }catch let err as NSError{
+            print("\(err)")
+        }
+    }
+
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        if let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "PokeCell", for: indexPath) as? PokeCell {
+            let pokemon = self.pokemon[indexPath.row]
+                cell.configureCell(pokemon: pokemon)
+            return cell
+            
+        }else{
+            
+            return UICollectionViewCell()
+        
+        }
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return pokemon.count
+    }
+    
+    func numberOfSections(in collectionView: UICollectionView) -> Int {
+        return 1
+        
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        return CGSize(width: 105, height: 105)
+    }
+}
+
